70525455e5fd215c01c20efa990e0093722cca9e
Luc de Cafmeyer <ldecafmeyer@gmail.com>
2022-10-04 21:43:51 +0200

    Fixed tsconfig.json paths + added redirect from login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,12 +1,28 @@
 import { NextPage } from "next";
 import LoginForm from "components/LoginForm";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useAuth } from "lib/AuthContext";
 
 const Page: NextPage = () => {
+  const { user, loading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) router.push("/");
+  }, [user, router]);
+
+  if (loading) {
+    return (
+      <h1 className="mt-10 text-5xl font-bold text-center">Loading...</h1>
+    );
+  }
+
   return (
     <main className="flex justify-center p-4">
       <LoginForm />
     </main>
-  )
-}
+  );
+};
 
 export default Page;
